a0af973b29e3141a23f5238fda25ced23fa3a302
zhangxianwen <1285163263@qq.com>
2019-07-29 19:47:52 +0800

    在dev加了helloworld3333333333333333

--- src/main/java/com/myself/test/HelloWorld.java
+++ src/main/java/com/myself/test/HelloWorld.java
@@ -3,6 +3,7 @@ package com.myself.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld00000000000");
-        System.out.println("HelloWorld11111111111111111");
+        System.out.println("HelloWorld1111111111111111");
+        System.out.println("HelloWorld222222222222222222222");
     }
 }
