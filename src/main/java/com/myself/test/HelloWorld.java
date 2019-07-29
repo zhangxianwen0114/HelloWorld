c1588bcfca4aedbdbbe1b783c3005f53587a3aad
zhangxianwen <1285163263@qq.com>
2019-07-29 19:32:43 +0800

    修改了helloworld

--- src/main/java/com/myself/test/HelloWorld.java
+++ src/main/java/com/myself/test/HelloWorld.java
@@ -2,6 +2,6 @@ package com.myself.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld00000000000");
     }
 }
